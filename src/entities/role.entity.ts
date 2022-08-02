b631bd83530daa74222d709560b1bea19a9862e4
Phan Khắc Đạo <daopk.dev@gmail.com>
2022-08-02 07:11:38 +0000

    Updated entities

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,5 +1,11 @@
-import { Entity } from '@mikro-orm/core';
+import { Entity, Property } from '@mikro-orm/core';
 import { BaseEntity } from '~/core/entities/base.entity';
 
 @Entity()
-export class Role extends BaseEntity {}
+export class Role extends BaseEntity {
+  @Property({ unique: 'roles.name_unique' })
+  name: string;
+
+  @Property()
+  isActive: boolean;
+}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,23 @@
-import { Entity } from '@mikro-orm/core';
+import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import { ApiHideProperty } from '@nestjs/swagger';
 import { WrappedEntity } from '~/core/entities/wrapped.entity';
+import { Role } from './role.entity';
 
 @Entity()
-export class User extends WrappedEntity<User> {}
+export class User extends WrappedEntity<User> {
+  @Property({ unique: 'users.username_unique' })
+  username: string;
+
+  @Property({ hidden: true })
+  @ApiHideProperty()
+  password: string;
+
+  @Property({ unique: 'users.email_unique' })
+  email: string;
+
+  @ManyToOne({ entity: () => Role, nullable: true })
+  role?: Role;
+
+  @Property()
+  isActive: boolean;
+}
