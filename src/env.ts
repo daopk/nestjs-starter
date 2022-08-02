5438baae676cb26cdc20f2db450793662190acbb
Phan Khắc Đạo <daopk.dev@gmail.com>
2022-08-02 05:39:45 +0000

    Added mikro orm & mongo

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,9 @@ export const env = defineEnv({
   HOST: str({ default: '0.0.0.0', devDefault: 'localhost' }),
   PORT: num({ default: 3000 }),
 
+  DB_TYPE: str({ default: 'mongo', choices: ['mongo', 'mysql', 'mariadb', 'postgresql', 'sqlite', 'better-sqlite'] }),
+  DB_URI: str({ desc: 'Database connection string' }),
+
   SWAGGER_PATH: str({ default: '/api/docs' }),
   SWAGGER_TITLE: str({ default: 'API' }),
 });
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Global, Module } from '@nestjs/common';
+import { env } from '~/env';
+
+@Global()
+@Module({
+  imports: [
+    MikroOrmModule.forRoot({
+      type: env.DB_TYPE,
+      clientUrl: env.DB_URI,
+      entities: ['dist/**/*.entity.js'],
+      debug: env.isDev,
+      ensureIndexes: true,
+      discovery: {
+        warnWhenNoEntities: false,
+      },
+    }),
+  ],
+})
+export class SharedModule {}
